c5eb4170837676edd654ea1473071f688ba9f3b3
emilyschrag <emily.schrag@ucalgary.ca>
2025-01-15 11:07:19 -0800

    added new contrast values

--- Masters-Unity/Assets/Scripts/ColorContrast.cs
+++ Masters-Unity/Assets/Scripts/ColorContrast.cs
@@ -56,19 +56,19 @@ namespace BCIEssentials.Utilities
         //Equal log steps 2, 3.91, 7.66, 15
         public Color Grey()
         {
-            //R/G/B = 218, Luminance = 205, 15.02:1, #DADADA
-            Color _maximum = new Color(0.8549f, 0.8549f, 0.8549f, 1f);
+            //R/G/B = 255
+            Color _maximum = new Color(1f, 1f, 1f, 1f);
 
-            //R/G/B = 156, Luminance = 147, 7.64:1, #9C9CBC
-            Color step1 = new Color(0.601176f, 0.6011176f, 0.601176f, 1f);
+            //R/G/B = 145
+            Color step1 = new Color(0.56862745f, 0.56862745f, 0.56862745f, 1f);
 
-            //R/G/B = 107, Luminance = 101, #6B6B6B
-            Color step2 = new Color(0.4196f, 0.4196f, 0.4196f, 1f);
+            //R/G/B = 82
+            Color step2 = new Color(0.32156863f, 0.32156863f, 0.32156863f, 1f);
 
-            //R/G/B = 63, L = 59, 1.99:1, #3F3F3F
-            Color _minimum = new Color(0.2471f, 0.2471f, 0.2471f, 1f);
+            //R/G/B = 47
+            Color _minimum = new Color(0.18431373f, 0.18431373f, 0.18431373f, 1f);
 
-            Color _off = new Color(0,0,0,0);
+            Color _off = new Color(0.00392157f, 0.00392157f, 0.00392157f,0);
 
             if(GetContrast() == 100)
             {
